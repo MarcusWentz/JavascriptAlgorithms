393714f761d0f8da2f085aa9274ccb6af73fa248
Marcus Wentz <52706599+MarcusWentz@users.noreply.github.com>
2022-01-30 19:06:27 -0500

    Update BinarySearchO(Log(n)).js

--- Algorithms/BinarySearchO(Log(n)).js
+++ Algorithms/BinarySearchO(Log(n)).js
@@ -1,22 +1,4 @@
-/**
- * Definition for isBadVersion()
- * 
- * @param {integer} version number
- * @return {boolean} whether the version is bad
- * isBadVersion = function(version) {
- *     ...
- * };
- */
-
-/**
- * @param {function} isBadVersion()
- * @return {function}
- */
 var solution = function(isBadVersion) {
-    /**
-     * @param {integer} n Total versions
-     * @return {integer} The first bad version
-     */
     return function(n) {
         let left = 1;
         let right = n;
@@ -29,6 +11,5 @@ var solution = function(isBadVersion) {
              }        
         }
         return left;
-        console.log(left)
     };
 };
