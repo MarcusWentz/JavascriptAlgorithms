a13aa56f526dfabc2cc96335e325c0dc5ff32156
Marcus Wentz <52706599+MarcusWentz@users.noreply.github.com>
2022-01-30 21:08:57 -0500

    Create BinaryTeeBreadthRightSide.js

---
+++ Algorithms/BinaryTeeBreadthRightSide.js
@@ -0,0 +1,24 @@
+var rightSideView = function(root) {
+    if(!root){
+        return []
+    }
+    
+    let queue = [root]
+    let result = []
+    
+    while(queue.length){
+        let queueLengthStored = queue.length
+        result.push(queue[queue.length-1].val) //Store right most value in result.
+        
+        while(queueLengthStored--){
+            let node = queue.shift() //Need to store queue as temporary nodes to review to push for not null values.
+            if(node.left){
+               queue.push(node.left)
+              }
+            if(node.right) {
+               queue.push(node.right)
+               }
+        }
+    }
+    return result
+};
