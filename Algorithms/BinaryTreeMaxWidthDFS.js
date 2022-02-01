d53b342bf7a9794323711d030f7cefde344675f2
Marcus Wentz <52706599+MarcusWentz@users.noreply.github.com>
2022-02-01 17:59:57 -0500

    Update BinaryTreeMaxWidthDFS.js

--- Algorithms/BinaryTreeMaxWidthDFS.js
+++ Algorithms/BinaryTreeMaxWidthDFS.js
@@ -2,22 +2,21 @@ var widthOfBinaryTree = function(root) {
     const minPos = [0];
     let maxWidth = 0;
     
-    function callDFS(node, level, pos) {
+    function dfs(node, level, pos) {
         if(!node) 
         {
-            return;
+            return null; //Same as just return.
         }
         if(minPos[level] === undefined){
             minPos.push(pos);
-        }
-        
+        }        
         const diff = pos - minPos[level];
         maxWidth = Math.max(maxWidth, diff+1);
         
-        callDFS(node.left, level+1, diff*2);
-        callDFS(node.right, level+1, diff*2+1);
+        dfs(node.left, level+1, diff*2);
+        dfs(node.right, level+1, diff*2+1);
     }
     
-    callDFS(root, 0, 0);
+    dfs(root, 0, 0);
     return maxWidth;
 };
