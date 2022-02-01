3119dc5365a3214ad70297ec06697d4247d71aab
Marcus Wentz <52706599+MarcusWentz@users.noreply.github.com>
2022-01-31 19:11:04 -0500

    Update and rename BinaryTreeDepthInvert.js to BinaryTreeInvert.js

--- Algorithms/BinaryTreeInvert.js
+++ Algorithms/BinaryTreeInvert.js
@@ -1,15 +1,14 @@
 var invertTree = function(root) {
     if(root === null) {
-        return root
+        return null
     }
     
     let storage = root.left
     root.left = root.right
     root.right = storage
         
-    invertTree(root.left)
-    invertTree(root.right)
+    invertTree(root.left) //Move all the way left until we hit a null.
+    invertTree(root.right) //Move all the way right until we hit a null.
     
     return root
-    
 };
