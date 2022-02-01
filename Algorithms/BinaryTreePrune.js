cdfab674723afbb72fdf76ad31e46c75c6098d64
Marcus Wentz <52706599+MarcusWentz@users.noreply.github.com>
2022-01-31 22:47:55 -0500

    Create BinaryTreePrune.js

---
+++ Algorithms/BinaryTreePrune.js
@@ -0,0 +1,19 @@
+var pruneTree = function(root) 
+{
+    if (root == null) { 
+        return null; 
+        }
+
+    root.left = pruneTree(root.left);
+    root.right = pruneTree(root.right);
+    
+    if (root.val == 0 && root.left == null && root.right == null)
+    {
+        return null;
+    }
+    else
+    {
+        return root;
+    }
+    
+};
